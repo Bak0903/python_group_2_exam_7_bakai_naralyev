907f2f38798cdf4c34e4194f207d941b596cc1a4
bak0903 <bak0903@mail.ru>
2019-02-09 16:33:32 +0600

    Написал функции для добавления, удаления и подчета суммы.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,112 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import AddItems from './Components/AddItems/AddItems';
+import OrderDetails from "./Components/OrderDetails/OrderDetails";
+
+// const TOTAL_PRICE = 0;
+//
+// const ITEMS=[];
 
 class App extends Component {
+
+  constructor(props) {
+        super(props);
+        this.state = {
+            items: [
+                {
+                  name: "Гамбургер", price: 80, className: "fas fa-utensils",
+                },
+                {
+                  name: "Кофе", price: 70, className: "fas fa-coffee",
+                },
+                {
+                  name: "Чисбургер", price: 90, className: "fas fa-utensils",
+                },
+                {
+                  name: "Чай", price: 50, className: "fas fa-coffee",
+                },
+                {
+                  name: "Фри", price: 45, className: "fas fa-utensils",
+                },
+                {
+                  name: "Кола", price: 40, className: "fas fa-coffee",
+                },
+            ],
+            orderDetails: [],
+            totalPrice: 0,
+        };
+    };
+
+  addFood = (id) => {
+    let orderDetails = this.state.orderDetails;
+    let food = "";
+    let foodId = this.state.orderDetails.findIndex(orderFood => {
+            return orderFood.id === id;
+        });
+    if (foodId < 0) {
+      food = this.state.items[id];
+      orderDetails.push(
+        {id: id, name: food.name, price: food.price, count: 1,}
+      );
+    }
+    else {
+      food = this.state.orderDetails[foodId];
+      console.log(food);
+      food.count = food.count + 1;
+      food.price = food.price + this.state.items[id].price;
+    }
+    let totalPrice = this.calculateTotalPrice();
+    this.setState({
+            ...this.state,
+            orderDetails,
+            totalPrice
+        }
+    );
+    console.log(this.state)
+  };
+
+  deleteFood = (id) => {
+    let orderDetails = this.state.orderDetails;
+    if (orderDetails[id].count > 1) {
+      let food = orderDetails[id];
+      food.count = food.count - 1;
+      food.price = food.price - this.state.items[id].price;
+    }
+    else {
+      orderDetails.splice(id, 1);
+    }
+    let totalPrice = this.calculateTotalPrice();
+    this.setState({
+        ...this.state,
+        orderDetails,
+        totalPrice
+        }
+    );
+  };
+
+  calculateTotalPrice = () => {
+    let totalPrice = 0;
+    console.log(totalPrice);
+    let orderFood = this.state.orderDetails;
+    for(var food in orderFood) {
+            totalPrice = totalPrice + orderFood[food].price;
+            console.log(totalPrice);
+        }
+    return totalPrice;
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <OrderDetails
+          total_price={this.state.totalPrice}
+          basket={this.state.orderDetails}
+          onDeleteFood={this.deleteFood}
+        />
+        <AddItems
+          menu={this.state.items}
+          onAddFood={this.addFood}
+        />
       </div>
     );
   }
---
+++ src/Components/AddItems/AddItems.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import './AddItems.css';
+import Item from './Item/Item';
+
+
+class AddItems extends Component {
+
+    render () {
+        return (
+            <div className={"add_items"}>
+                <h3 className={"title"}>Меню</h3>
+                <div className={'items'}>
+                    {
+                        Object.values(this.props.menu).map(
+                            (item, i) => {
+                                return (
+                                    <Item
+                                        key={i}
+                                        name={item.name}
+                                        price={item.price}
+                                        className={item.className}
+                                        onClickEvent={() => {
+                                            return this.props.onAddFood(i)
+                                        }}
+                                    />
+                                );
+                            }
+                        )
+                    }
+                </div>
+        </div>
+        );
+    }
+
+}
+
+export default AddItems;
